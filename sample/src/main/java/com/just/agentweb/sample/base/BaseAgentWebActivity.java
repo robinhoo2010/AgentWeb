434f27dd627c6d96b1d94c4d175e0728f6e676a2
2355282048 <2355282048@qq.com>
2021-02-26 18:04:33 +0800

    first try

--- sample/src/main/java/com/just/agentweb/sample/base/BaseAgentWebActivity.java
+++ sample/src/main/java/com/just/agentweb/sample/base/BaseAgentWebActivity.java
@@ -41,7 +41,7 @@ public abstract class BaseAgentWebActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // test
     }
 
     @Override
