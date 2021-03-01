8e56edea66cd55c9b3416d03280b5e272090e272
2355282048 <2355282048@qq.com>
2021-03-01 11:08:31 +0800

    core com.just.agentweb.WebChromeClient开放文件选择

--- agentweb-core/src/main/java/com/just/agentweb/MiddlewareWebChromeBase.java
+++ agentweb-core/src/main/java/com/just/agentweb/MiddlewareWebChromeBase.java
@@ -16,7 +16,12 @@
 
 package com.just.agentweb;
 
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 /**
  * @author cenxiaozhong
@@ -49,6 +54,47 @@ public class MiddlewareWebChromeBase extends WebChromeClientDelegate {
 
     final MiddlewareWebChromeBase next() {
         return this.mMiddlewareWebChromeBase;
+    }
+
+    @Override
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback,
+                                     FileChooserParams fileChooserParams) {
+        return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
+    }
+
+
+    /**
+     * Android  >= 4.1
+     *
+     * @param uploadFile
+     * @param acceptType
+     * @param capture
+     */
+    @Override
+    public void openFileChooser(ValueCallback<Uri> uploadFile, String acceptType, String capture) {
+        super.openFileChooser(uploadFile, acceptType, capture);
+    }
+
+    /**
+     * Android < 3.0
+     *
+     * @param valueCallback
+     */
+    @Override
+    public void openFileChooser(ValueCallback<Uri> valueCallback) {
+        super.openFileChooser(valueCallback);
+    }
+
+    /**
+     * Android  >= 3.0
+     *
+     * @param valueCallback
+     * @param acceptType
+     */
+    @Override
+    public void openFileChooser(ValueCallback valueCallback, String acceptType) {
+        super.openFileChooser(valueCallback, acceptType);
     }
 
 }
--- agentweb-core/src/main/java/com/just/agentweb/WebChromeClient.java
+++ agentweb-core/src/main/java/com/just/agentweb/WebChromeClient.java
@@ -1,11 +1,59 @@
 package com.just.agentweb;
 
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+
 /**
  * @author cenxiaozhong
  * @date 2019/4/13
  * @since 1.0.0
  */
 public class WebChromeClient extends MiddlewareWebChromeBase{
+
 	public WebChromeClient() {
+	}
+
+	@Override
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback,
+									 FileChooserParams fileChooserParams) {
+		return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
+	}
+
+
+	/**
+	 * Android  >= 4.1
+	 *
+	 * @param uploadFile
+	 * @param acceptType
+	 * @param capture
+	 */
+	@Override
+	public void openFileChooser(ValueCallback<Uri> uploadFile, String acceptType, String capture) {
+		super.openFileChooser(uploadFile, acceptType, capture);
+	}
+
+	/**
+	 * Android < 3.0
+	 *
+	 * @param valueCallback
+	 */
+	@Override
+	public void openFileChooser(ValueCallback<Uri> valueCallback) {
+		super.openFileChooser(valueCallback);
+	}
+
+	/**
+	 * Android  >= 3.0
+	 *
+	 * @param valueCallback
+	 * @param acceptType
+	 */
+	@Override
+	public void openFileChooser(ValueCallback valueCallback, String acceptType) {
+		super.openFileChooser(valueCallback, acceptType);
 	}
 }
